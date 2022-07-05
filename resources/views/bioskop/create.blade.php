5151242cdf421ddfc027050f5ab2ae4d3b710976
attamami <attamami1703@gmail.com>
2022-07-05 10:52:52 +0700

    UI Bioskop

--- resources/views/bioskop/create.blade.php
+++ resources/views/bioskop/create.blade.php
@@ -1,33 +1,115 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Tambah - Bioskop</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" > 
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Beranda - Bioskop</title>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <!-- Favicons -->
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{asset('vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+  <link href="{{asset('vendor/quill/quill.snow.css')}}" rel="stylesheet">
+  <link href="{{asset('vendor/quill/quill.bubble.css')}}" rel="stylesheet">
+  <link href="{{asset('vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+  <link href="{{asset('vendor/simple-datatables/style.css')}}" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{{asset('css/style.css')}}" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: NiceAdmin - v2.2.2
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
 </head>
+
 <body>
-    <div class="container mt-2">
-        <div class="row">
-            <div class="col-lg-12 margin-tb">
-                <div class="pull-left mb-2">
-                    <h3>Tambah</h3>
-                </div>
-                <div class="pull-right">
-                    <a class="btn btn-primary" href="{{ route('bioskop.index') }}"> Back</a>
-                </div>
-            </div>
-        </div>
-        @if(session('status'))
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top d-flex align-items-center">
+
+    <div class="d-flex align-items-center justify-content-between">
+      <a href="/" class="logo d-flex align-items-center">
+        <img src="" alt="">
+        <span class="d-none d-lg-block">Bioskopku</span>
+      </a>
+      <i class="bi bi-list toggle-sidebar-btn"></i>
+      <!-- <span class="d-none d-lg-block">Sumber Rejeki</span> -->
+    </div><!-- End Logo -->
+
+  </header>
+  <!-- End Header -->
+
+  <!-- ======= Sidebar ======= -->
+  <aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="/">
+          <i class="ri-home-2-fill"></i>
+          <span>Beranda</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="{{ route('film.index') }}">
+          <i class="ri-movie-fill"></i>
+          <span>Data Film</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="{{ route('bioskop.index') }}">
+          <i class="ri-slideshow-3-fill"></i>
+          <span>Data Bioskop</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="{{ route('tiket.index') }}">
+          <i class="ri-book-fill"></i>
+          <span>Tiket</span>
+        </a>
+      </li>
+
+    </ul>
+
+  </aside><!-- End Sidebar-->
+
+  <main id="main" class="main">
+
+<div class="pagetitle">
+  <h1>Data Bioskop</h1>
+  <nav>
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">Beranda</li>    
+        <li class="breadcrumb-item">Data Bioskop</li>
+        <li class="breadcrumb-item active">Tambah</li>
+      </ol>
+  </nav>
+</div><!-- End Page Title -->
+
+<section class="section">
+      <div class="row">
+        <div class="col-lg-12">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Data Bioskop</h5>
+      @if(session('status'))
             <div class="alert alert-success mb-1 mt-1">
                 {{ session('status') }}
             </div>
         @endif
 
-        <br>
         {{-- @foreach ($film as $key => $item)
             {{dd($item)}}
         @endforeach --}}
@@ -56,58 +138,75 @@
                 @endforeach
             @endif
         @endforeach --}}
-
         <form action="{{ route('bioskop.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
-            <div class="row">
+            <div class="form-email-php">
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
-                        <strong>Nama Bioskop:</strong>
+                        <strong>Nama Bioskop</strong>
                         <input type="text" id="nama_bioskop" name="nama_bioskop" class="form-control" placeholder="Masukkan Nama Bioskop" autocomplete="off">
                         @error('nama_bioskop')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                         @enderror
                     </div>
                 </div>
-
+<br>
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
-                        <strong>Nomor Telepon Bioskop:</strong>
+                        <strong>Nomor Telepon Bioskop</strong>
                         <input type="text" id="telepon_bioskop" name="telepon_bioskop" class="form-control" placeholder="Masukkan No Bioskop" autocomplete="off">
                         @error('telepon_bioskop')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                         @enderror
                     </div>
                 </div>
-
+<br>
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
-                        <strong>Alamat Bioskop:</strong>
-                        <input type="text" id="alamat_bioskop" name="alamat_bioskop" class="form-control" placeholder="Masukkan Alamat Bioskop" autocomplete="off">
+                        <strong>Alamat Bioskop</strong>
+                        <textarea id="alamat_bioskop" name="alamat_bioskop" class="form-control" placeholder="Masukkan Alamat Bioskop" autocomplete="off"></textarea>
                         @error('alamat_bioskop')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                         @enderror
                     </div>
                 </div>
+            <br>
 
-                <button type="submit" class="btn btn-primary ml-3">Submit</button>
+            <div class="">
+                <button type="submit" class="btn btn-primary ml-3">Submit</button> 
+                <a class="btn btn-primary" href="{{ route('bioskop.index') }}"> Back</a>
+            </div>
+                
             </div>
         </form>
+        
+        
     </div>
+              
+              <!-- End Table with stripped rows -->
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="{{asset('vendor/apexcharts/apexcharts.min.js')}}"></script>
+  <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('vendor/chart.js/chart.min.js')}}"></script>
+  <script src="{{asset('vendor/echarts/echarts.min.js')}}"></script>
+  <script src="{{asset('vendor/quill/quill.min.js')}}"></script>
+  <script src="{{ asset('vendor/simple-datatables/simple-datatables.js') }}"></script>
+  <script src="{{asset('vendor/tinymce/tinymce.min.js')}}"></script>
+  <script src="{{asset('vendor/php-email-form/validate.js')}}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{asset('js/main.js')}}"></script>
+
 </body>
-<script>
-    $(document).ready(function() {
 
-        // $(document).on('change', '#judul_film', function(){
-        //     // var res   =  $(this).find(':selected').data('row');
-        //     var res   =  $(this).find(':selected').data('row');
-        //     console.log(res);
-        //     // $('#rating_film').val('active');
-        //     // $('#genre_film').val('keisi');
-        //     $('#sutradara_film').val(res);
-        //     // $('#tahun_film').val('tahunnya');
-        // });
-
-    });
-</script>
 </html>
--- resources/views/tiket/index.blade.php
+++ resources/views/tiket/index.blade.php
@@ -2,29 +2,112 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <title>Beranda - Tiket</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" > 
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script> 
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Beranda - Tiket</title>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <!-- Favicons -->
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{asset('vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+  <link href="{{asset('vendor/quill/quill.snow.css')}}" rel="stylesheet">
+  <link href="{{asset('vendor/quill/quill.bubble.css')}}" rel="stylesheet">
+  <link href="{{asset('vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+  <link href="{{asset('vendor/simple-datatables/style.css')}}" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{{asset('css/style.css')}}" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: NiceAdmin - v2.2.2
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
 </head>
 
 <body>
-<div class="container">  
-  <div class="row">
-    <div class="col-lg-12 margin-tb">
-      <div class="pull-left">
-        <h3>Beranda - Tiket</h3>
-      </div>
-      <div class="pull-right">
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top d-flex align-items-center">
+
+    <div class="d-flex align-items-center justify-content-between">
+      <a href="/" class="logo d-flex align-items-center">
+        <img src="" alt="">
+        <span class="d-none d-lg-block">Bioskopku</span>
+      </a>
+      <i class="bi bi-list toggle-sidebar-btn"></i>
+      <!-- <span class="d-none d-lg-block">Sumber Rejeki</span> -->
+    </div><!-- End Logo -->
+
+  </header>
+  <!-- End Header -->
+
+  <!-- ======= Sidebar ======= -->
+  <aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="/">
+          <i class="ri-home-2-fill"></i>
+          <span>Beranda</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="{{ route('film.index') }}">
+          <i class="ri-movie-fill"></i>
+          <span>Data Film</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="{{ route('bioskop.index') }}">
+          <i class="ri-slideshow-3-fill"></i>
+          <span>Data Bioskop</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="{{ route('tiket.index') }}">
+          <i class="ri-book-fill"></i>
+          <span>Tiket</span>
+        </a>
+      </li>
+
+    </ul>
+
+  </aside><!-- End Sidebar-->
+
+  <main id="main" class="main">
+
+<div class="pagetitle">
+  <h1>Data Bioskop</h1>
+  <nav>
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">Beranda</li>    
+        <li class="breadcrumb-item active">Tiket</li>
+      </ol>
+  </nav>
+</div><!-- End Page Title -->
+
+<section class="section">
+      <div class="row">
+        <div class="col-lg-12">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Data Tiket</h5>
+              <div class="pull-right mb-2">
         <a class="btn btn-success" href="{{ route('tiket.create') }}"> Tambah</a>
       </div>
-    </div>
-  </div>
 
-  @if ($message = Session::get('success'))
+      @if ($message = Session::get('success'))
     <div class="alert alert-success">
       <p>{{ $message }}</p>
     </div>
@@ -59,6 +142,32 @@
   @endforeach
   </table>
   {!! $tiket->links() !!}
-</div>
+
+              
+              <!-- End Table with stripped rows -->
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="{{asset('vendor/apexcharts/apexcharts.min.js')}}"></script>
+  <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('vendor/chart.js/chart.min.js')}}"></script>
+  <script src="{{asset('vendor/echarts/echarts.min.js')}}"></script>
+  <script src="{{asset('vendor/quill/quill.min.js')}}"></script>
+  <script src="{{ asset('vendor/simple-datatables/simple-datatables.js') }}"></script>
+  <script src="{{asset('vendor/tinymce/tinymce.min.js')}}"></script>
+  <script src="{{asset('vendor/php-email-form/validate.js')}}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{asset('js/main.js')}}"></script>
+
 </body>
+
 </html>
